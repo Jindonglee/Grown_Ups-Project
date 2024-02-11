763335aaef06721770b4dcd2028299b2ad951c65
jindonglee <dropnowon@gmail.com>
2024-02-11 23:57:57 +0900

    feat : 네이버로그인 추가

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ import postsRouter from "./routes/post.router.js";
 import commentsRouter from "./routes/comment.router.js";
 import kakaoRouter from "./routes/kakao.router.js";
 import followRouter from "./routes/follow.router.js";
-import logMiddleware from './middlewares/log.router.js';
-import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
+import logMiddleware from "./middlewares/log.router.js";
+import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -18,7 +18,14 @@ app.use(logMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", [UsersRouter, postsRouter, commentsRouter, kakaoRouter, followRouter]);
+app.use("/api", [
+  UsersRouter,
+  postsRouter,
+  commentsRouter,
+  kakaoRouter,
+  followRouter,
+]);
+app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
--- src/routes/kakao.router.js
+++ src/routes/kakao.router.js
@@ -33,7 +33,6 @@ router.get("/kakao/userInfo", async (req, res, next) => {
     };
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    console.log(finalUrl);
     const kakaoTokenRequest = await fetch(finalUrl, {
       method: "POST",
       headers: {
@@ -90,10 +89,108 @@ router.get("/kakao/userInfo", async (req, res, next) => {
       res.cookie("refreshToken", refreshToken);
 
       // 유저 정보와 성공메시지를 반환
-      return res.json({ data: user, message: "로그인에 성공하셨습니다." });
+      return res.json({ message: "로그인에 성공하셨습니다." });
     } else {
       // 엑세스 토큰이 없으면 로그인페이지로 리다이렉트
       return res.redirect("/api/kakao/sign-in");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+let redirectURI = encodeURI("http://localhost:3000/api/naver/sign-in");
+
+router.get("/naver/sign-up", function (req, res) {
+  try {
+    const baseUrl = "https://nid.naver.com/oauth2.0/authorize";
+    const config = {
+      client_id: process.env.NAVER_ID,
+      redirect_uri: redirectURI,
+      response_type: "code",
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/naver/sign-in", async (req, res, next) => {
+  try {
+    const baseUrl = "https://nid.naver.com/oauth2.0/token";
+    const config = {
+      client_id: process.env.NAVER_ID, //restAPI 키
+      client_secret: process.env.NAVER_SECRET, //보안 키
+      grant_type: "authorization_code",
+      redirect_uri: redirectURI,
+      code: req.query.code,
+      state: req.query.state,
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const naverTokenRequest = await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        "X-Naver-Client-Id": process.env.NAVER_ID,
+        "X-Naver-Client-Secret": process.env.NAVER_SECRET,
+        "Content-type": "text/json;charset=utf-8",
+      },
+    });
+    const naverTokenData = await naverTokenRequest.json();
+
+    if ("access_token" in naverTokenData) {
+      const { access_token } = naverTokenData;
+      const userRequest = await fetch("https://openapi.naver.com/v1/nid/me", {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-type": "text/json;charset=utf-8",
+        },
+      });
+      const userData = await userRequest.json();
+      const user = await prisma.users.findFirst({
+        where: { email: userData.response.email },
+      });
+      // 카카오 로그인을 하지 않고 일반 가입한 사용자가 이메일이 같으면 오류코드를 띄워준다.
+      if (user && !user.naverId) {
+        return res.status(400).json({ message: "이미 가입한 이메일입니다." });
+      }
+      const gender = userData.response.gender === "M" ? "male" : "female";
+      if (!user) {
+        await prisma.users.create({
+          data: {
+            naverId: userData.response.id,
+            name: userData.response.name,
+            email: userData.response.email,
+            gender: gender,
+          },
+        });
+      }
+
+      // 엑세스 토큰과 리프레시 토큰 발급
+      const accessToken = jwt.sign(
+        { userId: user.userId },
+        process.env.ACCESS_TOKEN_SECRET_KEY,
+        {
+          expiresIn: "12h",
+        }
+      );
+      const refreshToken = jwt.sign(
+        { userId: user.userId },
+        process.env.REFRESH_TOKEN_SECRET_KEY,
+        {
+          expiresIn: "7d",
+        }
+      );
+
+      // 쿠키에 토큰 할당
+      res.cookie("authorization", `Bearer ${accessToken}`);
+      res.cookie("refreshToken", refreshToken);
+
+      // 유저 정보와 성공메시지를 반환
+      return res.json({ message: "로그인에 성공하셨습니다." });
+    } else {
+      // 엑세스 토큰이 없으면 로그인페이지로 리다이렉트
+      return res.redirect("/api/naver/sign-in");
     }
   } catch (error) {
     next(error);
