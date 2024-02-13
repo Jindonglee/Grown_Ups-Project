452f28661d0cbe9f90dce228af3eda82eeaf6252
Uhan33 <jazzcafe12@kangwon.ac.kr>
2024-02-13 10:32:05 +0900

    인증 미들웨어 쿠키로 변경
    req.user 는 userId만 넘기도록 하였습니다

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,35 +3,31 @@ import { prisma } from "../utils/prisma/index.js";
 
 export default async function (req, res, next) {
   try {
-    const authorization = req.headers.authorization;
-    if (!authorization) throw new Error("토큰이 존재하지 않습니다.");
+    const { authorization } = req.cookies;
+    if (!authorization)
+      throw new Error("요청한 사용자의 토큰이 존재하지 않습니다 .");
 
-    const [tokenType, tokenValue] = authorization.split(" ");
-
+    const [tokenType, token] = authorization.split(" ");
     if (tokenType !== "Bearer")
-      throw new Error("토큰 타입 Bearer 형식이 아닙니다.");
+      throw new Error("토큰 타입이 Bearer 형식이 아닙니다.");
 
-    if (!tokenValue) throw new Error("인증정보가 올바르지 않습니다.");
-
-    const token = jwt.verify(tokenValue, process.env.ACCESS_TOKEN_SECRET_KEY);
-
-    if (!token.userId) throw new Error("인증정보가 올바르지 않습니다.");
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+    const userId = decodedToken.userId;
+    if(!userId) throw new Error('로그인이 필요합니다.');
 
     const user = await prisma.users.findFirst({
-      where: { userId: token.userId },
+      where: { userId: +userId },
     });
-    if (!user) {
-      throw new Error("토큰 사용자가 존재하지 않습니다.");
-    }
+    if (!user) throw new Error("토큰 사용자가 존재하지 않습니다.");
 
-    // user 정보 담기
-    res.user = user;
-
+    req.user = user.userId;
     next();
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-      message: error.message,
-    });
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "토큰이 만료되었습니다." });
+    }
+    if (error.name === "JsonWebTokenError")
+      return res.status(401).json({ message: "토큰이 조작되었습니다." });
+    return res.status(400).json({ message: error.message });
   }
 }
